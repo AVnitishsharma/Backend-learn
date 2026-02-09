be1d5ee8ec142c343c740bbf7216a41984db24e6
Nitish Sharma <ernitishsharma361@gmail.com>
2026-02-09 15:55:11 +0530

    login and hash password done

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,22 +1,25 @@
 const express = require("express")
 const jwt = require("jsonwebtoken")
 const userModel = require("../models/user.model")
+const crypto = require("crypto")
 
-const authRoute = express.Router() //ye routh ko dusre file me challata ha
+const authRoute = express.Router() //ye routh ko dusre file me challata ha ka premination deta ha jaise app.js
 
-authRoute.post("/register", async (req , res)=>{
+authRoute.post("/register", async (req , res)=>{        //register api
   const {name, email, password} = req.body
 
   const isUserAllreadyExists = await userModel.findOne({email})  //ye same email ko find krta ha
 
   if(isUserAllreadyExists){       //ye same email pr error deta ha
-    res.status(400).json({
+    return res.status(409).json({
       message:"this email is allready exists"
     })
   }
 
+  const hash = crypto.createHash("md5").update(password).digest("hex")   // ye password ko hash password me create krta ha
+
   const user = await userModel.create({
-    name, email, password
+    name, email, password: hash
   })
 
   const token = jwt.sign(             //token create krte ha with id and email ap ek se bhi kr skte ha
@@ -32,6 +35,38 @@ authRoute.post("/register", async (req , res)=>{
   res.status(201).json({
     message:"user is created",
     user, token
+  })
+})
+
+authRoute.post("/login", async(req, res)=>{         //login api
+  
+  const {email, password} = req.body
+
+  const user = await userModel.findOne({email})  //email check krna ki phele se ye email ha bhi ya nhi
+  if(!user){
+    return res.status(404).json({
+      message:"email not found"
+    })
+  }
+ 
+  const isPasswordMatch = user.password === crypto.createHash("md5").update(password).digest("hex")   // user.password - database // ye password ko hash password me create krta ha
+  if(!isPasswordMatch){
+    return res.status(401).json({
+      message:"invilet password"
+    })
+  }
+
+  const token = jwt.sign(
+    {
+      id:user._id,
+      email:user.email,
+    },
+    process.env.JWT_SECRET
+  )
+
+  res.status(200).json({
+    message:"user login succesfully",
+    user,token
   })
 })
 
